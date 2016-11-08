ed7f47d18fa35cbcf0c7c584b8cf50a6ee52d22d
Nico44 <martin.nicolas44@gmail.com>
2016-11-08 09:35:01 +0100

    updated cachematrix.R and README.md

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+### Using the caching of the Inverse of a Matrix will enable to gain time for the user
 
-## Write a short comment describing this function
+
+# The first function makeCacheMatrix creates a special "vector", which is really a list containing a function to
+* 1. set the value of the matrix
+* 2. get the value of the matrix
+* 3. set the value of inverse of the matrix
+* 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setMatrixInverse <- function(solve) m <<- solve
+        getMatrixInverse <- function() m
+        list(set = set, get = get,
+             setMatrixInverse = setMatrixInverse,
+             getMatrixInverse = getMatrixInverse)
 }
 
 
-## Write a short comment describing this function
+
+# This function returns the inverse of the matrix. 
+# It will first check if the inverse was not already computed, if it was it will get the result directly from the cache
+# and skip the computation, if it was not already computed it will compute the inverse and store it in the cache thanks 
+# to our first function
+
+# Hypothesis: the matrix is always invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getMatrixInverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setMatrixInverse(m)
+        m
 }
+
+
+## Let's do a sample test!
+##> test <- diag(2,6)
+##> CachedMarix <- makeCacheMatrix(test)
+##> cacheSolve(CachedMarix) 
+##     [,1] [,2] [,3] [,4] [,5] [,6]
+##[1,]  0.5  0.0  0.0  0.0  0.0  0.0
+##[2,]  0.0  0.5  0.0  0.0  0.0  0.0
+##[3,]  0.0  0.0  0.5  0.0  0.0  0.0
+##[4,]  0.0  0.0  0.0  0.5  0.0  0.0
+##[5,]  0.0  0.0  0.0  0.0  0.5  0.0
+##[6,]  0.0  0.0  0.0  0.0  0.0  0.5
+
+
+
